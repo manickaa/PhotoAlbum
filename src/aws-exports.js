1e22336d834ae801d00ca5235439eddad0a511c4
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-09-29 12:33:35 -0700

    Added GraphQLAPIEndpoint information for config

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,6 +7,10 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_zeddpWLBk",
     "aws_user_pools_web_client_id": "16i4npi7n289p4uqqmihn58uv5",
+    "aws_appsync_graphqlEndpoint": "https://y65skph4wzhclnajwswivd2zbm.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "aws_appsync_apiKey": "null",
     "oauth": {}
 };
 
