9e2ac92dfdb7eedd5729862bba66aad920bf32bb
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-09-29 21:07:12 -0700

    Added subscription for updating the albums list when new album is created, without having to refresh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component}  from 'react';
 import {Connect} from 'aws-amplify-react';
-import {graphqlOperation} from 'aws-amplify';
+import {API, graphqlOperation} from 'aws-amplify';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
 import './App.css';
-import { Grid, Header, List, Segment } from 'semantic-ui-react';
+import { Grid, Header, Input, List, Segment } from 'semantic-ui-react';
 import '@aws-amplify/ui/dist/style.css';
 
 Amplify.configure(aws_exports);
@@ -20,7 +20,69 @@ const ListAlbums = `query ListAlbums {
   }
 }`;
 
+//Subscriptions - for subscribing to changes in data for real-time functionality like onCreate, onDelete, onUpdate
+const SubscribeToNewAlbum = `subscription onCreateAlbum {
+  onCreateAlbum {
+    id
+    name
+  }
+}`;
 
+//NewAlbum component to create new album and save it
+class CreateNewAlbum extends Component {
+  
+  //create a state to hold the albumName
+  constructor(props) {
+    super(props);
+    this.state = {
+      albumName: ''
+    };
+  }
+
+  //When the user gives an album name, update the state
+  editAlbumName = (event) => {
+    let change = {};
+    console.log(event.target.value)
+    change[event.target.name] = event.target.value;
+    this.setState(change);
+  }
+
+  //Asyn action to update DynamoDB using mutation
+  submitAlbumName = async(event) => {
+    //Prevent refreshing the page
+    event.preventDefault();
+    const NewAlbum = `mutation NewAlbum($name: String!) {
+      createAlbum(input: {name: $name}) {
+        id
+        name
+      }
+    }`;
+
+    //Log the created album's id to console
+    const result = await API.graphql(graphqlOperation(NewAlbum, {
+      name: this.state.albumName
+    }));
+    console.info(`Created album with id ${result.data.createAlbum.id}`);
+  }
+
+  render() {
+    return(
+      <Segment>
+        <Header as='h3'>Add New Album</Header>
+        <Input
+          type='text'
+          placeholder='New Album Name'
+          icon='plus'
+          iconPosition='left'
+          action={{content: 'Create', onClick: this.submitAlbumName}}
+          name='albumName'
+          value={this.state.albumName}
+          onChange={this.editAlbumName}
+        />
+      </Segment>
+    )
+  }
+}
 //Albumlist component for rendering a sorted list of album names
 class AlbumList extends Component {
   albumItems () {
@@ -45,16 +107,30 @@ class AlbumList extends Component {
 
 //AlbumListLoader component uses connect component from Amplify to provide data to AlbumList Component
 class AlbumListLoader extends Component {
+  //for handling onCreateAlbum sibscription
+  onNewAlbum = (prevQuery, newData) => {
+    //when we get data about a new album, we need to put it inot an object
+    //with the same shape as the original query but with the new data added as well
+
+    let updatedQuery = Object.assign({}, prevQuery);
+    updatedQuery.listAlbums.items = prevQuery.listAlbums.items.concat([newData.onCreateAlbum])
+    return updatedQuery;
+  }
   render() {
     return (
-      <Connect query={graphqlOperation(ListAlbums)}>
+      <Connect 
+          query={graphqlOperation(ListAlbums)}
+          subscription={graphqlOperation(SubscribeToNewAlbum)}
+          onSubscriptionMsg={this.onNewAlbum}
+      >
         {({data, loading, errors}) => {
           if(loading) {
             return <div>Loading...</div>
           }
-          console.log(data)
+          if(errors.length > 0) {
+            return <div>{JSON.stringify(errors)}</div>
+          }
           if(!data.listAlbums) {
-            console.log(errors)
             return;
           }
           return <AlbumList albums={data.listAlbums.items} />;
@@ -71,6 +147,7 @@ class App extends Component {
     return (
       <Grid padded>
         <Grid.Column>
+          <CreateNewAlbum />
           <AlbumListLoader/>
         </Grid.Column>
       </Grid>
